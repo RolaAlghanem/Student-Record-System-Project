99871505c3797ec14bf5120d04b2abf45a52ce4d
RolaAlghanem <451203244@qu.edu.sa>
2025-12-09 02:49:06 -0800

    Add files via upload

---
+++ StudentProject/src/thread/AutoSaveTask.java
@@ -0,0 +1,24 @@
+package thread;
+
+import manager.StudentManager;
+
+public class AutoSaveTask implements Runnable {
+
+    private StudentManager manager;
+
+    public AutoSaveTask(StudentManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                manager.saveAll();   // we will add this method next
+                Thread.sleep(5000);  // wait 5 seconds
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+}
